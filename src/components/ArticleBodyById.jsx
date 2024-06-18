26b23c8ce2341bccf5b65060831c0dcb5543941c
stefan <lioncatnewyork@gmail.com>
2024-06-18 14:39:28 +0100

    renders all comments related to an article id

--- src/components/ArticleBodyById.jsx
+++ src/components/ArticleBodyById.jsx
@@ -1,27 +1,53 @@
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from "react"
-import { fetchArticleById } from '../Api';
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchArticleById, fetchCommentsById } from "../Api";
 
 function ArticleBodyById() {
-    const { id } = useParams()
+  const { id } = useParams();
 
-    const [articleByID, setArticleById] = useState({})
-    
-    useEffect(() => {
-        fetchArticleById(id).then((data) => {
-            setArticleById(data)
+  const [isLoading, setIsLoading] = useState(false);
 
-    }, {})
-    })
+  const [articleById, setArticleById] = useState([]);
 
-    return (
-        <div>
-            <h3> {articleByID.title} </h3>
-            <h4> by: {articleByID.author}</h4>
-            <img  src={articleByID.article_img_url} alt="" />
-            <p> {articleByID.body} </p>
-        </div>
-    )
+  const [commentsById, setCommentsById] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchArticleById(id).then((data) => {
+      setArticleById(data);
+      setIsLoading(false);
+    });
+  }, [id]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchCommentsById(id).then((data) => {
+      setCommentsById(data.comments);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return !isLoading ? (
+    <div>
+      <h2> {articleById.title} </h2>
+      <h3> by: {articleById.author}</h3>
+      <img src={articleById.article_img_url} alt="" />
+      <p className="article-body"> {articleById.body} </p>
+      <h4>Comments: </h4>
+      <ul className="comments">
+        {commentsById.map((comment) => {
+          return (
+            <li key={comment.comment_id}>
+              <h5>{comment.author}:</h5>
+              <p>{comment.body}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  ) : (
+    <h3> Fetching Data ... </h3>
+  );
 }
 
-export default ArticleBodyById
+export default ArticleBodyById;

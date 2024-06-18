a3a930f0e248bb42d8262304ac18ffe6c96683ba
stefan <lioncatnewyork@gmail.com>
2024-06-18 10:05:19 +0100

    Sets allArticles with useState and passes it to child= AllArticles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import AllArticles from "./components/AllArticles";
+import { fetchAllArticles } from "../src/Api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState();
+  const [allArticles, setAllArticles] = useState([]);
+
+
+  useEffect(() => {
+    fetchAllArticles().then((data) => {
+      setAllArticles(() => {
+        return [...data];
+      });
+    });
+  }, [allArticles]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <BrowserRouter>
+        <Header user={user} setUser={setUser} />
+
+        <Routes>
+          <Route path="/" element={<Home allArticles={allArticles} />} />
+          <Route
+            path="/articles"
+            element={<AllArticles allArticles={allArticles} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
 
-export default App
+export default App;

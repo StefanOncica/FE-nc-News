9d4def615d47d747b67dac116b4f8fab7d848538
stefan <lioncatnewyork@gmail.com>
2024-06-20 10:26:49 +0100

    Adds new POST comment request

--- src/Api.js
+++ src/Api.js
@@ -11,25 +11,36 @@ export const fetchAllUsers = () => {
 };
 
 export const fetchAllArticles = () => {
-  return newsApi.get(`/articles`).then(({data})=>{
-    return data.articles
-  })
-}
+  return newsApi.get(`/articles`).then(({ data })=> {
+    return data.articles;
+  });
+};
 
 export const fetchArticleById = (id) => {
-  return newsApi.get(`/articles/${id}`).then(({data})=>{
-    return data
-  })
-}
+  return newsApi.get(`/articles/${id}`).then(({ data }) => {
+    return data;
+  });
+};
 
 export const fetchCommentsById = (id) => {
-  return newsApi.get(`/articles/${id}/comments`).then(({data})=>{
-    return data
-  })
-}
+  return newsApi.get(`/articles/${id}/comments`).then(({ data }) => {
+    return data;
+  });
+};
 
 export const updateVotes = (id, incVotes) => {
-  return newsApi.patch(`/articles/${id}`, {inc_votes: incVotes}).then(({data}) => {
-    return data
-  })
-}
+  return newsApi
+    .patch(`/articles/${id}`, { inc_votes: incVotes })
+    .then(({ data }) => {
+      return data;
+    });
+};
+
+export const postComment = (id, body, user) => {
+  return newsApi
+    .post(`/articles/${id}/comments`, {
+      username: user,
+      body: body
+    })
+    
+};

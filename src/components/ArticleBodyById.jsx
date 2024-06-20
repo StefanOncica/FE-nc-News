43b64138ec05b55d4795ebddcc61d1e4e5fbd55e
stefan <lioncatnewyork@gmail.com>
2024-06-20 10:55:15 +0100

    Task 8 - adds posting comment functionality

--- src/components/ArticleBodyById.jsx
+++ src/components/ArticleBodyById.jsx
@@ -1,8 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { fetchArticleById, fetchCommentsById, updateVotes } from "../Api";
+import {
+  fetchArticleById,
+  fetchCommentsById,
+  postComment,
+  updateVotes,
+} from "../Api";
 
-function ArticleBodyById() {
+function ArticleBodyById({ user }) {
   const { id } = useParams();
 
   const [isLoading, setIsLoading] = useState(false);
@@ -13,6 +18,10 @@ function ArticleBodyById() {
 
   const [votes, setVotes] = useState();
 
+  const [commentInput, setCommentInput] = useState("");
+
+  const [newCommentExists, setNewCommentExists] = useState(1);
+
   useEffect(() => {
     setIsLoading(true);
     fetchArticleById(id).then((data) => {
@@ -20,17 +29,37 @@ function ArticleBodyById() {
       setVotes(data.votes);
       setIsLoading(false);
     });
-
-    fetchCommentsById(id).then((data) => {
-      setCommentsById(data.comments);
-      setIsLoading(false);
-    });
   }, []);
 
+  useEffect(() => {
+    fetchCommentsById(id).then((data) => {
+      setCommentsById(data.comments);
+    });
+  }, [newCommentExists]);
+
   function handleLike(event) {
+    event.preventDefault();
     const incVote = Number(event.target.value);
     setVotes((currentVotes) => currentVotes + incVote);
     updateVotes(id, incVote);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (commentInput) {
+      postComment(id, commentInput, user).then((response) => {
+        if (response.status === 201) {
+          setNewCommentExists((curr) => {
+            return curr + 1;
+          });
+        }
+      });
+      setCommentInput("");
+    }
+  }
+
+  function handleChange(event) {
+    setCommentInput(event.target.value);
   }
 
   return !isLoading ? (
@@ -39,16 +68,34 @@ function ArticleBodyById() {
       <h3> by: {articleById.author}</h3>
       <img src={articleById.article_img_url} alt="" />
       <p className="article-body"> {articleById.body} </p>
-      <label>
-        {votes} people have liked this article.
+      <label>{votes} people have liked this article.</label>
+
+      <div className="like-dislike-buttons">
         <button onClick={handleLike} value={1}>
           Like
         </button>
         <button onClick={handleLike} value={-1}>
           Dislike{" "}
         </button>
-      </label>
+      </div>
       <h4>Comments: </h4>
+      <form onSubmit={handleSubmit} className="comment-form">
+        <p>
+          {user
+            ? "Type your comment bellow."
+            : "You must sign in before you can post a comment."}
+        </p>
+        <textarea
+          onChange={handleChange}
+          value={commentInput}
+          type="text"
+          required
+        ></textarea>
+        <div>
+          <button> Add comment</button>
+        </div>
+      </form>
+
       <ul className="comments">
         {commentsById.map((comment) => {
           return (
